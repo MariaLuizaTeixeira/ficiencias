a3bcab64f64b483bd67096764bf33b145c3890c3
Maria Luiza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2026-05-07 17:50:23 -0300

    fest: create class User

---
+++ backend/api/src/App/Models/User.php
@@ -0,0 +1,87 @@
+<?php
+namespace app\Models;
+
+use Cassandra\Date;
+use DateTime;
+
+class User {
+    // atributos
+    private int $id;
+    private string $fullName;
+    private string $email;
+    private string $passwordHash;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+    private bool $emailVerified;
+
+    // getters e setters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getFullName(): string
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName(string $fullName): void
+    {
+        $this->fullName = $fullName;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash(string $passwordHash): void
+    {
+        $this->passwordHash = $passwordHash;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function isEmailVerified(): bool
+    {
+        return $this->emailVerified;
+    }
+
+    public function setEmailVerified(bool $emailVerified): void
+    {
+        $this->emailVerified = $emailVerified;
+    }
+}
