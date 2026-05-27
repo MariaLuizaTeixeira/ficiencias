a94db805eecb8801e30632101773fbe821675d91
Maria Luiza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2026-05-27 18:05:18 -0300

    wip: student dao

--- backend/Dao/StudentDAO.php
+++ backend/Dao/StudentDAO.php
@@ -2,7 +2,36 @@
 
 namespace Dao;
 
-class StudentDAO
-{
+use Model\Student;
+use Util\Conexao;
 
+class StudentDAO {
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Conexao::getConexao();
+    }
+
+    public function listar(): array {
+        $sql = "SELECT * FROM students";
+        $stm = $this->conexao->prepare($sql);
+        $stm->execute();
+        return $stm->fetchAll();
+    }
+
+    public function criar(Student $student): void {
+
+
+        $sql = "INSERT INTO students (id, lives, streak, birth_date, nickname, gender, phone_number, profile_image_url) VALUES (?, ?, ?, ?, ?, ?)";
+
+        $stm = $this->conexao->prepare($sql);
+        $stm->execute([$student->getId(), $student->getLives(), $student->getStreak(), $student->getBirth(), $student->getNickname(), $student->getGender(), $student->getPhoneNumber(), $student->getProfilePicture()]);
+    }
+
+    public function deletar(int $id): void {
+        $sql = "DELETE FROM students WHERE id = ?";
+
+        $stm = $this->conexao->prepare($sql);
+        $stm->execute([$id]);
+    }
 }
