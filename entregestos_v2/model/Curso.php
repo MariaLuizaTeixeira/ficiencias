acb456c1675c9c6debb71d07b7affbf8bd3c30a8
brenda14k <brendam.ifpr2024@gmail.com>
2026-08-23 17:52:52 -0300

    Create Curso.php

---
+++ entregestos_v2/model/Curso.php
@@ -0,0 +1,50 @@
+<?php
+
+class Curso
+{
+    private ?int $id;
+    private ?string $nome;
+    private ?string $descricao;
+    private ?array $secoes;
+
+    public function __toString()
+    {
+        return $this->nome . " - " . $this->descricao;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(?string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getDescricao(): ?string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(?string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+}
